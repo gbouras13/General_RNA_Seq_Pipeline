1684a385b10afae6ff9eb249205cf673c42ebf1e
gbouras13 <george.bouras@adelaide.edu.au>
2022-05-06 10:08:20 +0930

    slurm mem star2

--- slurm/star_create_indices.sh
+++ slurm/star_create_indices.sh
@@ -10,10 +10,10 @@
 
  # Resources allocation request parameters
  #SBATCH -p batch
- #SBATCH -N 1                                                    # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
- #SBATCH -c 1                                                    # number of cores (sequential job calls a multi-thread program that uses 8 cores)
- #SBATCH --time=12:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
- #SBATCH --mem=1GB                                              # specify memory required per node
+ #SBATCH -N 1                                    
+ #SBATCH -c 1                                                    
+ #SBATCH --time=12:00:00                                   
+ #SBATCH --mem=1GB                                             
 
 
  SNK_DIR="/hpcfs/users/a1667917/Kevin/RNA_Seq_Deconv_Pipeline"
